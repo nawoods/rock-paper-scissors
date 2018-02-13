a596f2ec6836e8fe1f627d09283e55174e293d68
nawoods <nicholas.andrew.woods@gmail.com>
2018-02-13 23:53:29 +0000

    Add round counters and message for end of game

--- script.js
+++ script.js
@@ -9,24 +9,24 @@ function computerPlay() {
     return "scissors";
 }
 
-function playerPlay() {
-    let selection;
-    while (true) {
-        selection = prompt("Enter rock, paper, or scissors");
-        selection = selection.toLowerCase();
-        if (selection === "rock" || selection === "paper" || selection === "scissors") {
-            break;
-        }
-        alert("Sorry, try again");
-    }
-    return selection;
-}
 
 function capitalize(string) {
     let begin = string.slice(0,1).toUpperCase();
     let end = string.slice(1).toLowerCase();
     return begin + end;
 }
+
+
+function addWinLosses(round) {
+    if (round.slice(0, 5) === "You w") {
+        wins++;
+    } else if (round.slice(0, 5) === "You l") {
+        losses++;
+    } else {
+        draws++;
+    }
+}
+
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
@@ -43,27 +43,45 @@ function playRound(playerSelection, computerSelection) {
         return "You win! " + capitalize(playerSelection) + " beats " + computerSelection;
     }
     return "You lose! " + capitalize(computerSelection) + " beats " + playerSelection;
-    
 }
+
 
 function selectPlay(selection) {
     const round = playRound(selection, computerPlay());
+    addWinLosses(round);
     
     gameResult.innerHTML = round;
+    score.innerHTML = "Wins: " + wins + " Losses: " + losses + " Draws: " + draws;
     
-//    if (round.slice(0, 5) === "You w") {
-//        
-//    }
+    if (wins + losses + draws === 5) {
+        if (wins > losses) {
+            matchResult.innerHTML = "Game over! You win!";
+        } else if (losses > wins) {
+            matchResult.innerHTML = "Game over! The computer wins!";
+        } else {
+            matchResult.innerHTML = "Game over! It's a tie!";
+        }
+        
+        wins = 0;
+        losses = 0;
+        draws = 0;
+    } else {
+        matchResult.innerHTML = "";
+    }
 }
+
 
 let wins = 0;
 let losses = 0;
+let draws = 0;
 
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
 
 const gameResult = document.querySelector('#game-result');
+const score = document.querySelector('#score');
+const matchResult = document.querySelector('#match-result');
 
 rock.addEventListener('click', () => selectPlay("rock"));
 paper.addEventListener('click', () => selectPlay("paper"));
